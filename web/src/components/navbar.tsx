cdb2fee4a89f0595870d91510384b8539bebbf3c
atzaytsev <atzaytsev@avito.ru>
2025-02-03 01:47:10 +0500

    remove boilerplate branding

--- web/src/components/navbar.tsx
+++ web/src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@heroui/button";
+// import { Button } from "@heroui/button";
 import { Kbd } from "@heroui/kbd";
 import { Link } from "@heroui/link";
 import { Input } from "@heroui/input";
@@ -17,13 +17,13 @@ import clsx from "clsx";
 import { siteConfig } from "@/config/site.ts";
 import { ThemeSwitch } from "@/components/theme-switch.tsx";
 import {
-  TwitterIcon,
+  // TwitterIcon,
   GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
+  // DiscordIcon,
+  // HeartFilledIcon,
   SearchIcon,
 } from "@/components/icons.tsx";
-import { Logo } from "@/components/icons.tsx";
+// import { Logo } from "@/components/icons.tsx";
 
 export const Navbar = () => {
   const searchInput = (
@@ -56,8 +56,8 @@ export const Navbar = () => {
             color="foreground"
             href="/web/public"
           >
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            {/*<Logo />*/}
+            <p className="font-bold text-inherit">Wildfire Detection</p>
           </Link>
         </NavbarBrand>
         <div className="hidden lg:flex gap-4 justify-start ml-2">
@@ -83,30 +83,30 @@ export const Navbar = () => {
         justify="end"
       >
         <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal href={siteConfig.links.twitter} title="Twitter">
-            <TwitterIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.discord} title="Discord">
-            <DiscordIcon className="text-default-500" />
-          </Link>
-          <Link isExternal href={siteConfig.links.github} title="GitHub">
-            <GithubIcon className="text-default-500" />
-          </Link>
+          {/*<Link isExternal href={siteConfig.links.twitter} title="Twitter">*/}
+          {/*  <TwitterIcon className="text-default-500" />*/}
+          {/*</Link>*/}
+          {/*<Link isExternal href={siteConfig.links.discord} title="Discord">*/}
+          {/*  <DiscordIcon className="text-default-500" />*/}
+          {/*</Link>*/}
+          {/*<Link isExternal href={siteConfig.links.github} title="GitHub">*/}
+          {/*  <GithubIcon className="text-default-500" />*/}
+          {/*</Link>*/}
           <ThemeSwitch />
         </NavbarItem>
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-        <NavbarItem className="hidden md:flex">
-          <Button
-            isExternal
-            as={Link}
-            className="text-sm font-normal text-default-600 bg-default-100"
-            href={siteConfig.links.sponsor}
-            startContent={<HeartFilledIcon className="text-danger" />}
-            variant="flat"
-          >
-            Sponsor
-          </Button>
-        </NavbarItem>
+        {/*<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>*/}
+        {/*<NavbarItem className="hidden md:flex">*/}
+        {/*  <Button*/}
+        {/*    isExternal*/}
+        {/*    as={Link}*/}
+        {/*    className="text-sm font-normal text-default-600 bg-default-100"*/}
+        {/*    href={siteConfig.links.sponsor}*/}
+        {/*    startContent={<HeartFilledIcon className="text-danger" />}*/}
+        {/*    variant="flat"*/}
+        {/*  >*/}
+        {/*    Sponsor*/}
+        {/*  </Button>*/}
+        {/*</NavbarItem>*/}
       </NavbarContent>
 
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
